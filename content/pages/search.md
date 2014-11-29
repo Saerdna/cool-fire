dd2f15d3ceaa206c567947ef76a13f911db416c6
saerdna <zhaodahao@gmail.com>
2014-11-30 00:41:29 +0800

    modify

---
+++ content/pages/search.md
@@ -0,0 +1,17 @@
+title: Search
+date: 2013-08-26 11:36:58
+slug: search
+
+<script>
+  (function() {
+    var cx = '002523504798099060974:2rxs_rgrdd0';
+    var gcse = document.createElement('script');
+    gcse.type = 'text/javascript';
+    gcse.async = true;
+    gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
+        '//www.google.com/cse/cse.js?cx=' + cx;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(gcse, s);
+  })();
+</script>
+<gcse:search></gcse:search>
---
+++ pelicanconf.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Saerdna'
+SITENAME = u'cool-fire'
+#SITEURL = 'http://saerdna.github.io/'
+
+USE_FOLDER_AS_CATEGORY = True
+
+TIMEZONE = 'Asia/Shanghai'
+#NUM_FULL_ARTICLES = 100
+DEFAULT_LANG = u'zh'
+DEFAULT_DATE_FORMAT = '%Y-%m-%d'
+#THEME = 'bootstrap2'
+RELATIVE_URLS = True
+
+
+ARCHIVES_URL = 'archives.html'
+ARTICLE_URL = 'posts/{date:%Y}-{date:%m}-{date:%d}/{slug}.html'
+ARTICLE_SAVE_AS = 'posts/{date:%Y}-{date:%m}-{date:%d}/{slug}.html'
+
+DISQUS_SITENAME = 'coolfire'
+GOOGLE_ANALYTICS = 'UA-43474657-1'
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+# Blogroll
+LINKS =  (('Pelican', 'http://getpelican.com/'),
+          ('Python.org', 'http://python.org/'),
+          ('Jinja2', 'http://jinja.pocoo.org/'),
+          ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('http://weibo.com/saerdna13', 'Weibo'),
+        ('https://plus.google.com/105858506209398295278', 'Google plus'),)
+
+DEFAULT_PAGINATION = 5
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
